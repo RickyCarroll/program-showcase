5b6c2e6db29ed94f0806e0cc14a260feedbec59e
gonzalm7 <gonzalm7@wwu.edu>
2018-05-02 12:54:34 -0700

    test

--- users.js
+++ users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router =  express.Router();
 
 var User = require('./models/user');
+console.log('Added some console message');
 
 /* createAccount '/createAccount' */
 router.get('/', function (req, res) {
