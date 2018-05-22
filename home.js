5f7eb64f2a5f3f317208b9ff9a4548790d152bcd
gonzalm7 <gonzalm7@wwu.edu>
2018-05-21 20:29:10 -0700

    Updated layout. User pages don't have proper css...

--- home.js
+++ home.js
@@ -4,7 +4,6 @@ var router =  express.Router();
 var User = require('./models/user');
 
 
-
 /* home page '/' */
 router.get('/', function (req, res) {
     /* you can render objects and array with send(object). */
@@ -25,24 +24,7 @@ router.get('/logout', function(req, res){
     res.redirect('/');
 });
 
-router.post('/', function (req, res) {
-    console.log('searching');
-    User.getUserByUsername(req.body.username, function (err,user) {
-        console.log("searching");
-        console.log(user);
-        if (err) { throw err}
-        else if (user != null) {
-            res.render('accountPage', {
-                user: user
-            });
-        } else {
-            res.render('home', {
-                title: "Program Showcase",
-            })
-        }
-    });
-    
-})
+
 
 module.exports = router;
 /* login */
