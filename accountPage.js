3350486813333d0bad7b832c3fa6b22a87eb2f58
zachacash <cashz@wwu.edu>
2018-11-26 18:52:26 -0800

    cleaned up the code and add comments in server.js and accountPage.js

--- accountPage.js
+++ accountPage.js
@@ -9,33 +9,14 @@ var del = require('del');
 var User = require('./models/user');
 var sess;
 
-//
-
-
+/* used by every link that is covered by the accountPage
+* to makes sure that you are login and are the correct person on your account. */
 function loggedIn(req, res, next) {
-
-    console.log('the url- ' + req.url);
-
-    /*var word_list = req.url.split('/');
-    var username = '';*/
-    // var username = str.join('/');
-
-    /*if (word_list.length > 1) {
-        username = req.url.replace('/',"").toString();
-    } else {
-        username = word_list[0]
-    }*/
-
     var str = req.url.replace('/','').split('/');
-
-
     var username = str[0];
 
-    console.log('str - ' + str);
-    console.log('middle ware - ' + username);
     if (req.session) {
         if (req.session.userName === username) {
-            console.log("the req.user " + req.session + '\n');
             next();
         } else {
             res.redirect('/login');
@@ -44,29 +25,24 @@ function loggedIn(req, res, next) {
 
         res.redirect('/login');
     }
-
-
-
 }
 
-
+/* looks in a file to see if it contains the input function in
+* python. It then returns the string in each input function. */
 function checkForInput(filename) {
     var data = fs.readFileSync(filename);
-
     var wordsArray = data.toString().split(/\s+/);
     var input_statements = [];
 
     for (var i=0; i<wordsArray.length; i++) {
         if (wordsArray[i].includes('input(')) {
             var statement = wordsArray[i].substring(wordsArray[i].indexOf('input(') + 6,wordsArray[i].length).replace('\'','').replace('\"','');
-            console.log("statement: " + statement);
             if (wordsArray[i].includes(')')) {
                 statement = statement.substring(0, statement.indexOf(')')).replace('\'','').replace('\"','');
             } else {
                 var offset = 1;
                 var endParath = false;
                 while (i+offset < wordsArray.length && !endParath) {
-                    console.log("the word: " + wordsArray[i+offset]);
                     if (wordsArray[i+offset].includes(')')) {
                         endParath = true;
                         statement = statement + wordsArray[i+offset].replace(')','').replace('\'','').replace('\"','');
@@ -84,19 +60,15 @@ function checkForInput(filename) {
 
 router.use(fileUpload());
 
+/* handle the user's home account page */
 router.get('/:username', loggedIn, function (req, res) {
-//router.get('/:username', function (req, res) {
-    //console.log('go to the account page');
-    //var username = req.params.username;
-    //var user = User.getUser(username);
     var username = req.url.replace('/',"").toString();
     User.getUser(username, function (err, user) {
-        console.log("got user");
         if (err) throw err;
         else if (user != null) {
+            /* get all the programs for the user */
             Program.getAllProgramsByUsername(username, function(err,programs) {
                 if (err) throw err;
-                console.log(programs);
                 res.render('accountPage',{
                     user : user,
                     programs: programs
@@ -108,29 +80,26 @@ router.get('/:username', loggedIn, function (req, res) {
 
 /* handle the logout*/
 router.get('/:username/logout', loggedIn, function(req, res, next) {
-    console.log("we made it to the logout!");
     sess = req.session;
-
-        if (sess) {
+    if (sess) {
         // delete session object
         sess.destroy(function(err) {
             if(err) {
                 return next(err);
             } else {
                 return res.redirect('/');
-
             }
         });
     }
 });
 
+/* handle the user's page for uploading a program. */
 router.get('/:username/upload', loggedIn, function (req, res) {
     //console.log('go to the account page');
     //var username = req.params.username;
     //var user = User.getUser(username);
     var username = req.url.replace('/',"").toString();
     User.getUser(username, function (err, user) {
-        console.log("got user");
         if (err) throw err;
         res.render('upload',{
             user : user
@@ -138,43 +107,37 @@ router.get('/:username/upload', loggedIn, function (req, res) {
     });
 });
 
+/* allows the user to upload a program along with a title.
+* It does some security checks such as making sure the uploaded file is
+* a .py file and the file doesn't import os. */
 router.post('/:username/upload', function (req,res) {
     if (!req.files)
         return res.status(400).send('No files were uploaded.');
 
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-
     var username = req.url.split('/')[1];
-
     var user = {username : username};
-
     var sampleFile = req.files.sampleFile;
     var progName = req.body.progName;
     var progTitle = progName;
-
     var bad_input = false;
     var errors;
 
-
+    /* The following if statements check to see if the user filled in all the required input boxes */
     if (sampleFile != null) {
         if (sampleFile.name.indexOf(".py") === -1) {
             errors = ["Please upload a python file."];
             bad_input = true;
         }
     }
-
     if (sampleFile == null) {
         errors = ['Please choose a Python file.'];
         bad_input = true;
     }
-
     if (progName === '') {
         errors = ['Please enter a name for your program that you are uploading.'];
         bad_input = true;
     }
-
     if (bad_input) {
-        console.log("error");
         res.render('upload',{
             user : user,
             error: errors
@@ -182,37 +145,23 @@ router.post('/:username/upload', function (req,res) {
         return;
     }
 
-
     /* remove any spaces in the name */
     progName = progName.replace(/\s+/g, '');
-    console.log(progName + " the program's name");
 
     /* check the program name to avoid repeated titles */
-
-    var newProg = null;
-
     Program.getProgramByUsername(username,progName, function (err,programDetails) {
         if (programDetails) {
-            console.log(programDetails + ' match');
-
             /* msg to the user! */
-            req.checkBody('progName', ' please enter a different name.').equals(!progName);
+            req.checkBody('progName', ' please enter a different name.').equals(!progName);  //TODO:
             var errors = ['That title is already used by another one of your programs.'];
-
-            console.log("error");
             res.render('upload',{
                 user : user,
                 error: errors
             });
         } else {
-
             var path = __dirname + '/views/Users/' + username + '/' + progName;
-
-
-
             fs.mkdir(path, function (err) {
                 if (err) {
-                    console.log('failed to create directory', err);
                     res.render('upload', {
                         user: user
                     })
@@ -222,45 +171,32 @@ router.post('/:username/upload', function (req,res) {
                         if (err)
                             return res.status(500).send(err);
 
-                        //res.send('File uploaded!');
-                        console.log(path + '/' + sampleFile.name);
-
                         var statements = checkForInput(path+'/'+sampleFile.name);
-
-                        console.log(statements);
-
-                        newProg = new Program({
+                        var newProg = new Program({
                             username: username,
                             programName: progName,
                             programTitle: progTitle,
                             mainProgram: sampleFile.name,
                             inputMessages: statements
                         });
-                        console.log(newProg);
-
+                        /* look in the uploaded file for "import os". */
                         var content = fs.readFileSync(path + '/' + sampleFile.name);
-
                         if (err) throw err;
-                        console.log('2');
                         if(content.indexOf('import os') >= 0) {
-                            console.log("why is this happening");
-                            console.log("they used os!");
                             bad_file = true;
                             errors = ["you can't upload a python file that imports os."];
                             del([path]);
-                            console.log('username and progName \'' + username + "\' and \'" + progName + '\'');
                             res.render('upload',{
                                 user : user,
                                 error: errors
                             });
                             return;
                         }
+                        /* add the programs info to the DB. */
                         Program.addProgram(newProg, function (err, newProg) {
                             if (err) console.log(err);
                             req.flash('success', 'Added Program');
-                            console.log(newProg);
                         });
-                        console.log('4');
                         res.redirect('/account/' + username);
                     });
                 }
@@ -271,23 +207,17 @@ router.post('/:username/upload', function (req,res) {
 
 });
 
+/* handles the running of a program. */
 router.get('/:username/run/:progName', loggedIn, function (req, res) {
-    // console.log(req.url);
-    // var name = req.url.substr(1, req.url.length - 9);
     var list = req.url.split('/');
-    console.log(list);
     var username = list[1];
     var progName = list[3];
-    console.log("bring up run page" + username + " " + progName);
-
+    /* get the program from the DB via the username and the name of the program. */
     Program.getProgramByUsername(username,progName,function (err, programDetails) {
         if (err) throw err;
-
         if (!programDetails) {
             res.redirect('/account' + username);
         } else {
-            console.log(programDetails);
-
             res.render('console',{
                 program : programDetails,
                 username: username,
@@ -299,41 +229,27 @@ router.get('/:username/run/:progName', loggedIn, function (req, res) {
 
 router.get('/:username/remove/:progName', loggedIn, function (req,res) {
     var list = req.url.split('/');
-    console.log(list);
     var username = list[1];
     var progName = list[3];
-
+    /* remove the program from the DB. */
     Program.removeProgram(username,progName, function(err,deleted_program){
         if (err) throw err;
         if (deleted_program === null) {
-            console.log("error: couldn't delete program");
         } else {
-            console.log("the program has been deleted.");
-            console.log(deleted_program);
             var path = __dirname + '/views/Users/' + username + '/' + progName;
             del([path + '/' + deleted_program.mainProgram]);
             del([path]);
         }
     });
-
     res.redirect('/account/' + username);
-
 });
 
 router.get('/', function (req, res) {
-    /* you can render objects and array with send(object). */
-    /* render takes an .ejs file and renders it*/
-    //db.users.find(function (err,docs) {
-    //console.log(req.user);
-    //console.log(req.isAuthenticated());
     var userSession = res.locals.session;
     res.render('home',{
         title: "Program Showcase",
         user:userSession
-        //users: docs
     });
-    //console.log(docs);
-    //});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,6 @@
 
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 /* This is used to validate the user input.
  * this requires some middle ware */
@@ -15,47 +14,42 @@ var expressValidator = require('express-validator');
 var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var mongo = require('mongodb');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/codex');
-var db = mongoose.connection;
 
 var home = require('./home');
 var users = require('./users');
 var projects = require('./projects');
 var login = require('./login');
 var accountPage = require("./accountPage");
-// var runPage = require("./runPage");
 var search = require('./searchProjects');
 var about = require('./about');
-// var jquery = require('jquery');
-// var fileUpload = require('express-fileupload');
 var help = require('./help');
 
 var IGNORE = '5qFcRqWexG5uuqDiWeRHCPEvECYmDDUxzbAM1zNK';
 
 var app = express();
+
+
+
 /* this is middle ware and it is used to preform a task
  * in between the request and the response.
- * it was access to both the request and the response and
+ * it has access to both the request and the response and
  * it will run every time the application is loaded.
 */
-
-
 /* View Engine */
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
-app.engine('html', require('ejs').renderFile);
+app.engine('html', require('ejs').renderFile); /* allows us to use ejs */
 
 /* Body Parser Middleware*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-// app.use(cookieParser());
 
 /* Set Static Path to templates which is where the html/css is. */
 app.use(express.static(path.join(__dirname,'views')));
 
+/* define a session to use for logged in users. */
 app.use(session({
     name: 'yummy_cookie',
     secret: IGNORE,
@@ -68,12 +62,9 @@ app.use(session({
     }
 }));
 
-
-
-/* Passport init */
+/* Passport init used for login */
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 /* Express validator error formatter. */
 app.use(expressValidator());
@@ -89,6 +80,7 @@ app.use(function (req,res,next) {
     next();
 });
 
+/* the files that are responsible for the different urls on the site. */
 app.use('/', home);
 app.use('/createAccount', users);
 app.use('/projectsPage', projects);
@@ -99,10 +91,12 @@ app.use('/about', about);
 app.use('/help', help);
 app.use('/searchProjects', search);
 
+/* start the server up */
 app.listen(8000,function () {
     console.log('Server Started on Port 8000...');
 });
 
+/* the error page that comes up if there is an invalid url. */
 app.use(function (req, res, next) {
     res.status(404).render('404.ejs');
 });
